3f9b08e48dab383142bf3fbe481d10a7ae0234e1
cptuulia <cptuulia@users.noreply.github.com>
2026-01-05 12:56:29 +0100

    add own theme

---
+++ wp-content/themes/worpressdemo/functions.php
@@ -0,0 +1,9 @@
+<?
+/*
+function add_stylesheet_to_head() {
+   // var_dump(get_stylesheet_uri()); die;
+    //  echo "<link href='".get_stylesheet_uri()."' rel='stylesheet' type='text/css'>";
+}
+
+add_action( 'wp_head', 'add_stylesheet_to_head' );
+*/  
